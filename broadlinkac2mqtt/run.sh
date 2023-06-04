d36b36f903d255ac04066e80eb3be459e0fe655c
Artem Vladimirov <artem.vladimirov@agropoisk.ru>
2023-06-04 13:16:33 -0300

    First release

--- broadlinkac2mqtt/run.sh
+++ broadlinkac2mqtt/run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 python3 -c 'import sys, yaml, json; print(yaml.dump(json.loads(open("/data/options.json").read())))' > /broadlinkac2mqtt/config/config.yaml
-
-cd /broadlinkac2mqtt
+ls
 
 ## Run your program
-./broadlinkac2mqtt
+/broadlinkac2mqtt/broadlinkac2mqtt

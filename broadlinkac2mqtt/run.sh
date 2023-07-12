2c82eb4c4650b09330a5eb2b27f34c1feeaf7395
Artem Vladimirov <artem.vladimirov@agropoisk.ru>
2023-07-12 16:11:16 -0300

    First release

--- broadlinkac2mqtt/run.sh
+++ broadlinkac2mqtt/run.sh
@@ -4,7 +4,8 @@ pwd
 ls
 ls -l ./broadlinkac2mqtt
 python3 -c 'import sys, yaml, json; print(yaml.dump(json.loads(open("/data/options.json").read())))' > /broadlinkac2mqtt/config/config.yaml
-pwd
-ls
-ls -l ./broadlinkac2mqtt
+FILE=/broadlinkac2mqtt/broadlinkac2mqtt
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+fi
 /broadlinkac2mqtt/broadlinkac2mqtt
